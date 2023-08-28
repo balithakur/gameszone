22b266322fdc6ebc930a2e716d0901cb59a007c5
bali singh <balisc04@gmail.com>
2023-08-28 21:07:47 +0530

    modify views file

--- firstApp/views.py
+++ firstApp/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def landingpage(request):
     return render(request, 'landingpage.html')
+
+
 def home(request):
     #return HttpResponse("this is homepage")
     return render(request, 'navbar.html')
